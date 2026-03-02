da26ce67cb610d7d7db7ff705dfef2f20eac9adc
Zakary Strange <zakarystrange@gmail.com>
2026-03-02 15:53:33 -0800

    Remove unused tempaltes and add missing uassets

--- Templates/TP_TopDown/Source/TP_TopDown/TP_TopDownGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TP_TopDownGameMode.h"
-
-ATP_TopDownGameMode::ATP_TopDownGameMode()
-{
-	// stub
-}
--- Templates/TP_VehicleAdv/Source/TP_VehicleAdv/TP_VehicleAdv.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TP_VehicleAdv.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_VehicleAdv, "TP_VehicleAdv" );
-
-DEFINE_LOG_CATEGORY(LogTP_VehicleAdv)
