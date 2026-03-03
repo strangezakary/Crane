97470bcd835f7f3d9b39f119b36b28e27a11c4ef
andirew <andirew.karate@gmail.com>
2026-03-02 16:51:19 -0800

    Gantry Crane Base logic and Possession/Unpossession

---
+++ Crane/Source/Crane/Gameplay/Private/GantryCrane.cpp
@@ -0,0 +1,178 @@
+﻿#include "Gameplay/Public/GantryCrane.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+
+AGantryCrane::AGantryCrane()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CraneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("CraneRoot"));
+	SetRootComponent(CraneRoot);
+
+	BridgeMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BridgeMesh"));
+	BridgeMesh->SetupAttachment(CraneRoot);
+	BridgeMesh->SetSimulatePhysics(false);
+
+	TrolleyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TrolleyMesh"));
+	TrolleyMesh->SetupAttachment(BridgeMesh);
+
+	HookMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HookMesh"));
+	HookMesh->SetupAttachment(TrolleyMesh);
+
+	PossessVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("PossessVolume"));
+	PossessVolume->SetupAttachment(CraneRoot);
+
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+}
+
+void AGantryCrane::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BridgeWorldPos = BridgeMesh->GetComponentLocation();
+	TrolleyRelPos = FVector::ZeroVector;
+	HookRelZ = HookMaxZ;
+
+	PossessVolume->OnComponentBeginOverlap.AddDynamic(this, &AGantryCrane::OnPossessVolumeOverlap);
+	HookMesh->SetRelativeLocation(FVector(0.f, 0.f, HookRelZ));
+}
+
+void AGantryCrane::ApplyPhysics(float& Velocity, float Input, float Accel,
+                                  float MaxSpeed, float Damping, float DeltaTime)
+{
+	if (!FMath::IsNearlyZero(Input))
+	{
+		Velocity += Input * Accel * DeltaTime;
+		Velocity = FMath::Clamp(Velocity, -MaxSpeed, MaxSpeed);
+	}
+	else
+	{
+		Velocity = FMath::FInterpTo(Velocity, 0.f, DeltaTime, Damping);
+	}
+}
+
+void AGantryCrane::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//bridge
+	ApplyPhysics(BridgeVelocity, BridgeInput,BridgeAcceleration, BridgeMaxSpeed, BridgeDamping, DeltaTime);
+
+	BridgeWorldPos.X += BridgeVelocity * DeltaTime;
+	BridgeWorldPos.X = FMath::Clamp(BridgeWorldPos.X,GetActorLocation().X - BridgeRailHalfLength,GetActorLocation().X + BridgeRailHalfLength);
+	BridgeMesh->SetWorldLocation(BridgeWorldPos);
+
+	//trolley
+	ApplyPhysics(TrolleyVelocity, TrolleyInput,TrolleyAcceleration, TrolleyMaxSpeed, TrolleyDamping, DeltaTime);
+
+	TrolleyRelPos.Y += TrolleyVelocity * DeltaTime;
+	TrolleyRelPos.Y = FMath::Clamp(TrolleyRelPos.Y, -TrolleyHalfLength, TrolleyHalfLength);
+	TrolleyMesh->SetRelativeLocation(TrolleyRelPos);
+
+	//Hook Winch
+	float WinchDir = bLoweringHook? -1.f: 1.f; // -Z = down
+	HookRelZ += WinchDir * WinchSpeed * DeltaTime;
+	HookRelZ = FMath::Clamp(HookRelZ, HookMinZ, HookMaxZ);
+	HookMesh->SetRelativeLocation(FVector(0.f, 0.f, HookRelZ));
+}
+
+void AGantryCrane::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (!EIC) return;
+
+	if (IA_Move)
+	{
+		EIC->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AGantryCrane::OnMove);
+	}
+
+	if (IA_Winch)
+	{
+		EIC->BindAction(IA_Winch, ETriggerEvent::Started, this, &AGantryCrane::OnWinchPressed);
+		EIC->BindAction(IA_Winch, ETriggerEvent::Completed, this, &AGantryCrane::OnWinchReleased);
+	}
+	if (IA_Unpossess)
+		EIC->BindAction(IA_Unpossess, ETriggerEvent::Started, this, &AGantryCrane::OnUnpossess);
+}
+
+void AGantryCrane::OnMove(const FInputActionValue& Value)
+{
+	FVector2D MoveAxis = Value.Get<FVector2D>();
+	BridgeInput  = MoveAxis.Y;// W/S = bridge forward/back
+	TrolleyInput = MoveAxis.X;// A/D = trolley left/right
+}
+
+void AGantryCrane::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	APlayerController* PC = Cast<APlayerController>(NewController);
+	if (PC)
+	{
+		PreviousPawn = PC->GetPawn();
+	}
+}
+
+void AGantryCrane::OnPossessVolumeOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+                                          bool bFromSweep, const FHitResult& SweepResult)
+{
+	APawn* OverlappingPawn = Cast<APawn>(OtherActor);
+	if (!OverlappingPawn) return;
+
+	APlayerController* PC = Cast<APlayerController>(OverlappingPawn->GetController());
+	if (!PC) return;
+
+	APawn* CachedPawn = OverlappingPawn;
+	PC->Possess(this);
+	PreviousPawn = CachedPawn;
+	if (ULocalPlayer* LP = PC->GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (IMC_Crane)
+			{
+				Subsystem->AddMappingContext(IMC_Crane, 1);
+			}
+		}
+	}
+}
+
+void AGantryCrane::OnUnpossess()
+{
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC && PreviousPawn)
+	{
+		PC->Possess(PreviousPawn);
+		PreviousPawn = nullptr;
+	}
+}
+
+void AGantryCrane::OnBridgeForward(float Val)
+{
+	BridgeInput = Val;
+}
+
+void AGantryCrane::OnTrolleyRight(float Val)
+{
+	TrolleyInput = Val;
+}
+
+void AGantryCrane::OnWinchPressed()
+{
+	bLoweringHook = true;
+}
+
+void AGantryCrane::OnWinchReleased()
+{
+	bLoweringHook = false;
+}
---
+++ Crane/Source/Crane/Gameplay/Public/GantryCrane.h
@@ -0,0 +1,115 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GantryCrane.generated.h"
+
+class UInputMappingContext;
+
+UCLASS()
+class AGantryCrane : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    AGantryCrane();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    void PossessedBy(AController* NewController);
+    void OnUnpossess();
+	void OnMove(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void OnPossessVolumeOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Crane|Components")
+    UStaticMeshComponent* BridgeMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Crane|Components")
+    UStaticMeshComponent* TrolleyMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Crane|Components")
+    UStaticMeshComponent* HookMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Crane|Components")
+    USceneComponent* CraneRoot;
+
+	UPROPERTY()
+	TObjectPtr<APawn> PreviousPawn;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Crane|Components")
+	TObjectPtr<UBoxComponent> PossessVolume;
+
+	UPROPERTY(EditAnywhere, Category = "Crane|Input")
+	TObjectPtr<UInputMappingContext> IMC_Crane;
+
+	UPROPERTY(EditAnywhere, Category = "Crane|Input")
+	TObjectPtr<UInputAction> IA_Move;
+
+	UPROPERTY(EditAnywhere, Category = "Crane|Input")
+	TObjectPtr<UInputAction> IA_Winch;
+
+	UPROPERTY(EditAnywhere, Category = "Crane|Input")
+	TObjectPtr<UInputAction> IA_Unpossess;
+	
+    //Bridge 
+    UPROPERTY(EditAnywhere, Category = "Crane|Bridge")
+    float BridgeAcceleration = 300.f;
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Bridge")
+    float BridgeMaxSpeed = 500.f;
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Bridge")
+    float BridgeDamping = 4.f;
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Bridge")
+    float BridgeRailHalfLength = 1000.f; //limit
+
+    //Trolley
+    UPROPERTY(EditAnywhere, Category = "Crane|Trolley")
+    float TrolleyAcceleration = 250.f;
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Trolley")
+    float TrolleyMaxSpeed = 400.f;
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Trolley")
+    float TrolleyDamping = 4.f;
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Trolley")
+    float TrolleyHalfLength = 600.f; // Relative to bridge center ±Y
+
+    //Hook (Space)
+    UPROPERTY(EditAnywhere, Category = "Crane|Hook")
+    float WinchSpeed = 350.f; //cm/s
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Hook")
+    float HookMinZ = -800.f; //Lowest relative position
+
+    UPROPERTY(EditAnywhere, Category = "Crane|Hook")
+    float HookMaxZ = 0.f; //highest (retracted)
+
+private:
+    float BridgeVelocity = 0.f; //along X
+    float TrolleyVelocity = 0.f; //along Y 
+
+    float BridgeInput = 0.f; //-1 to 1, from W/S
+    float TrolleyInput = 0.f; //-1 to 1, from A/D
+    bool bLoweringHook = false; 
+
+    FVector BridgeWorldPos;
+    FVector TrolleyRelPos;
+    float HookRelZ;
+
+    void ApplyPhysics(float& Velocity, float Input, float Accel, float MaxSpeed, float Damping, float DeltaTime);
+
+    void OnBridgeForward(float Val);
+    void OnTrolleyRight(float Val);
+    void OnWinchPressed();
+    void OnWinchReleased();
+};
